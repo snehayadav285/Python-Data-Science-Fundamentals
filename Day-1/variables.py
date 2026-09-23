79cb9b5ff74a231061a2a18d9806988f755ee411
snehayadav285 <snehaydv0303@gmail.com>
2026-09-23 18:25:57 +0530

    Add Day 1 variables practice

---
+++ Day-1/variables.py
@@ -0,0 +1,13 @@
+# Day 1 - Python Variables
+
+# Creating variables
+name = "Sneha"
+age = 20
+course = "B.Tech"
+is_student = True
+
+# Displaying variables
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+print("Student:", is_student)
