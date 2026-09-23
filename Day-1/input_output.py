67d9f827a0c6d52dff0d1d9c9764335c0a10d2b3
snehayadav285 <snehaydv0303@gmail.com>
2026-09-23 18:29:17 +0530

    Add input and output example in Python

---
+++ Day-1/input_output.py
@@ -0,0 +1,9 @@
+# Day 1 - Python Input and Output
+
+# Taking input from the user
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Displaying the entered information
+print("Hello,", name)
+print("Your age is:", age)
