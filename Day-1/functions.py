19d6316f56766b98664467550f659a0ef1923cf1
snehayadav285 <snehaydv0303@gmail.com>
2026-09-23 18:37:02 +0530

    Add functions for greeting and addition in Python

---
+++ Day-1/functions.py
@@ -0,0 +1,18 @@
+# Day 1 - Python Functions
+
+# Defining a function
+def greet(name):
+    print("Hello,", name)
+
+
+# Calling the function
+greet("Sneha")
+
+
+# Function with a return value
+def add(a, b):
+    return a + b
+
+
+result = add(10, 5)
+print("Sum:", result)
