b0b6b2950f4d951ca90ccae88963b4818cb475aa
snehayadav285 <snehaydv0303@gmail.com>
2026-09-23 18:35:26 +0530

    Add loops.py with examples of for and while loops

---
+++ Day-1/loops.py
@@ -0,0 +1,17 @@
+# Day 1 - Python Loops
+
+# For loop
+print("For loop:")
+
+for i in range(1, 6):
+    print(i)
+
+
+# While loop
+print("While loop:")
+
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
